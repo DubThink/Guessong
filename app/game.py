8b5cb6b0b3c495ea94201fbe15e3724e9adf6216
Benjamin Welsh <benjaminwelsh2@gmail.com>
2018-11-29 17:12:59 -0500

    Added state ticking

--- app/game.py
+++ app/game.py
@@ -1,5 +1,7 @@
 from app import spotifyapi
 from difflib import SequenceMatcher
+import threading
+import time
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
@@ -16,6 +18,10 @@ GUESS_INCORRECT = 'incorrect'
 GUESS_CLOSE = 'close'
 GUESS_CORRECT = 'correct'
 
+WAITING=0
+ROUND_LIVE=1
+ROUND_END=2
+GAME_END=3
 
 class Game:
     playlistID = None
@@ -27,6 +33,10 @@ class Game:
     roomID = "tacocode"
     gameStarted = False
     playlistData = None
+
+    # game state data
+    startTime=0
+    state=WAITING
 
     def __init__(self,roomcode):
         self.roomID=roomcode
@@ -83,22 +93,35 @@ class Game:
     def getPlayersData(self):
         return {{"name":n.name,"score":n.score} for n in self.gameUsers.items()}
 
+    def startRound(self):
+        self.state=ROUND_LIVE
+        self.startTime = time.time()
+
+        # TODO next song
+
+    def finishRound(self):
+        self.state=ROUND_END
+        self.startTime = time.time()
+
 
 class GameManager:
     roomToGame = {}
+    ticking=False
+    updateClients=None
 
     def getGame(self, key):
         return self.roomToGame[key]
 
     def createGame(self):
-        game = Game()
+        game = Game("bobb")
         self.roomToGame[game.getRoomCode()] = game
         return game
         
     def startGame(self, key):
         if key in self.roomToGame:
             return False
-        self.roomToGame[key] = Game(key)
+        self.roomToGame[key].nextRound()
+        self.startTicking()
         return True
 
     def endGame(self, key):
@@ -106,4 +129,33 @@ class GameManager:
             return False
         self.getGame(key).endGame()
         self.roomToGame.pop(key)
+        if len(self.roomToGame) is 0:
+            self.stopTicking()
         return True
+
+    def startTicking(self):
+        print('starting to tick')
+        if not self.ticking:
+            self.ticking=True  # enable ticking
+            self._updateTick()  # start ticking
+
+    def stopTicking(self):
+        self.ticking=False
+
+    def _updateTick(self):
+        if self.ticking:
+            threading.Timer(1, self._updateTick).start()
+        print('ticking...')
+        for roomcode, game in self.roomToGame.items():
+            if game.state is ROUND_LIVE and time.time()-game.startTime>30:
+                game.finishRound()
+                if self.updateClients:
+                    self.updateClients(roomcode, game)
+            elif game.state is ROUND_END and time.time()-game.startTime>15:
+                game.startRound()
+                if self.updateClients:
+                    self.updateClients(roomcode, game)
+                return
+
+
+
